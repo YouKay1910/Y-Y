fea11c123df72da045f0d23be815e0fde948ca06
宮田雄介 <ysk379.skrmj@gmail.com>
2018-01-20 19:41:31 +0800

    commit

--- yusuke/leave.php
+++ yusuke/leave.php
@@ -1,10 +1,12 @@
 <?php
-	if (!isset($_POST['select'])) {
-		header('Location: ../yoka/login.php');
-		exit();
-	}
+		// if (!isset($_POST['select'])) {
+		// 	header('Location: ../yoka/login.php');
+		// 	exit();
+		// }
+	$reason = '';
 
 	if (!empty($_POST)) {
+		$reason = $_POST['reason'];
 		$errors = array();
 		if (!isset($_POST['select'])) {
 			$errors['select'] = 'noSelect';
@@ -40,34 +42,36 @@
 	<meta charset="utf-8">
 	<title>退会手続き画面</title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="leave.css">
 </head>
 <body>
-	<h1 style="text-align: center; padding-top:30px;">退会手続き</h1>
+	<h1>退会手続き</h1>
 
 	<?php if(isset($errors['select']) && $errors['select'] == 'noSelect'): ?>
-		<div class="alert alert-danger" style="text-align: center;">
+		<div class="alert alert-danger">
 				退会理由を1つだけ選択してください
 		</div>
 	<?php endif ?>
 
 
-	<div style="text-align: center; margin-top: 100px;">
+	<div>
 		<p>退会理由を下記から選択してください</p>
 	</div>
 	<form method="POST" action="#">
 		<div>
-			<ul style="display: table; padding: 0; margin: 20px auto;">
-				<li style="display: table;"><input type="checkbox" name="select">必要ない</li>
-				<li style="display: table;"><input type="checkbox" name="select">飽きた</li>
-				<li style="display: table;"><input type="checkbox" name="select">モチベーションがあがらない</li>
+			<ul>
+				<li><input type="checkbox" name="select">必要ない</li>
+				<li><input type="checkbox" name="select">飽きた</li>
+				<li><input type="checkbox" name="select">モチベーションがあがらない</li>
+				<li><input type="checkbox" name="select">その他</li>
 			</ul>
 		</div>
-		<div style="text-align: center;">
-			<p style="margin-bottom: 20px;">その他ご意見・ご要望があったら書いてね</p>
-			<textarea style="width: 200px; height: 100px;" name="reason"></textarea>
+		<div>
+			<p class="opinion">その他ご意見・ご要望があったら書いてね</p>
+			<textarea class="reason" name="reason" value ="<?php echo $reason; ?>"></textarea>
 		</div>
-		<div style="text-align: center; margin-top: 50px">
-			<input type="submit" name="leave" value="退会申請する">
+		<div style="margin-top: 50px">
+			<input type="submit" value="退会申請する">
 		</div>
 	</form>
 </body>
